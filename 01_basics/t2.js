df1ef6327fb99584cfe6a104ba53f4204bbcebf8
rohits2404 <119805960+rohits2404@users.noreply.github.com>
2024-03-19 07:46:52 +0000

    save changes

---
+++ 01_basics/t2.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "rohit@yahoo.com"
+var accountPassword = "67854"
+accountCity = "Delhi"
+
+//accountId = 2
+console.log(accountId);
+
+accountEmail = "rs2121@gmail.com";
+accountPassword = "1234";
+accountCity = "Pune";
+
+console.table([accountEmail,accountPassword,accountCity]);
