8bad8d2bb758d00fd13481d52b85ab9e0d9cbb4a
rohits2404 <119805960+rohits2404@users.noreply.github.com>
2024-03-19 15:29:46 +0000

    new changes

---
+++ 02_datatypes/t2.js
@@ -0,0 +1,15 @@
+// let num1 = "10";
+
+// console.log(typeof num1);
+
+// let num2 = Number(num1);
+// console.log(typeof num2);
+
+// ****************  OPERATIONS  ***********************
+
+let val = (0.1+0.2).toFixed(1);
+console.log(val);
+
+console.log("1"+2);             // 12
+console.log("1"+2+2);           //  122  If string is first then all will be treated as String
+console.log(2+2+"1"+2);         //  412  If String is Last then operations are executed first
