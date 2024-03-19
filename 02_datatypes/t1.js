dd027ebc8f3b69cb65e2a1c6ed01c99ebe6f07d9
rohits2404 <119805960+rohits2404@users.noreply.github.com>
2024-03-19 14:46:06 +0000

    new change

---
+++ 02_datatypes/t1.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all JS code as newer version
+
+let str = "Rohit";
+let age = 18;
+let isLoggedIn = true;
+
+console.log(typeof str)
+
+let val1 = null;
+console.log(typeof val1);
