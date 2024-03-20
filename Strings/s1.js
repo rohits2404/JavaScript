a121acd75e31d36a2f59286c84c38def9e3ee124
rohits2404 <119805960+rohits2404@users.noreply.github.com>
2024-03-20 13:51:06 +0000

    new file

---
+++ Strings/s1.js
@@ -0,0 +1,22 @@
+let str = 'Rohit';
+console.log(str);
+
+// DO NOT USE + OPERATOR FOR STRING CONCATENATION
+
+console.log(`hello my name is ${str} and my ${'repocount'} is 1`);
+
+const val = new String('hey there is a boat standing whose name is titanic!')
+
+console.log(val.length);
+console.log(val.charAt(7));
+console.log(val.charCodeAt(7));
+console.log(val.codePointAt(7));
+console.log(val.concat(str));
+console.log(val.endsWith('titanic'));
+console.log(val.includes('rohit'));
+console.log(val.indexOf('r'));
+console.log(val.lastIndexOf('t'));
+
+
+
+
